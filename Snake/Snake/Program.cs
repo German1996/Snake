e6c35ff70d6d5bd40a8098d674073d68347a2b1e
german <german1996@yandex.ru>
2016-02-21 12:09:55 +0300

    Листы

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -17,6 +17,44 @@ namespace Snake
             Point p2 = new Point(4,5,'%');
             p2.Draw();
 
+            Point p3 = new Point(1, 2, '#');
+            Point p4 = new Point(6, 7, '^');
+
+            List<int> nunList = new List<int>();
+            nunList.Add(0);
+            nunList.Add(1);
+            nunList.Add(2);
+
+            int x = nunList[0];
+            int y = nunList[1];
+            int z = nunList[2];
+
+            foreach(int i in nunList)
+            {
+                Console.WriteLine(i);
+            }
+
+            nunList.RemoveAt(0);
+
+            List < Point > pList= new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+            
+
+            List<char> sym = new List<char>();
+            sym.Add('*');
+            sym.Add('#');
+            sym.Add('^');
+
+            foreach (char symbol in sym)
+            {
+                Console.WriteLine(symbol);
+            }
+
+
             Console.ReadLine();
 
         }
